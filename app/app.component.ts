ccacb5288a0f7a5df02ba7eda3026c306c0f2b7d
Maksim Fomin <hellow68@gmail.com>
2015-12-16 23:14:09 +0300

    Tutorial finished (yet?)

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,7 +25,9 @@ var HEROES: IHero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="#hero of heroes" (click)="onSelect(hero)">
+            <li *ngFor="#hero of heroes"
+                (click)="onSelect(hero)"
+                [class.selected]="hero === selectedHero">
                  <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
