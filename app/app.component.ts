34621887a7710ea24fb3abae3d1d862a304b65bf
Maksim Fomin <hellow68@gmail.com>
2015-12-16 23:10:46 +0300

    Hero selecting

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,25 +25,47 @@ var HEROES: IHero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="#hero in heroes">
+            <li *ngFor="#hero of heroes" (click)="onSelect(hero)">
                  <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-        <h2>Hero {{hero.name}} details!</h2>
-        <div>
-            <label>ID:</label>{{hero.id}}
-        </div>
-        <div>
-            <label>Name: </label>
+        <div *ngIf="selectedHero">
+            <h2>Hero {{selectedHero.name}} details!</h2>
             <div>
-                <input [(ngModel)]="hero.name" placeholder="Name of hero">
+                <label>ID:</label>{{selectedHero.id}}
+            </div>
+            <div>
+                <label>Name:</label>
+                <div>
+                    <input [(ngModel)]="selectedHero.name" placeholder="Name of hero">
+                </div>
             </div>
         </div>
-    `
+    `,
+    styles:[`
+      .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+      .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+      .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+      .heroes .badge {
+        font-size: small;
+        color: white;
+        padding: 0.1em 0.7em;
+        background-color: #369;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -1px;
+      }
+      .selected { background-color: #EEE; color: #369; }
+    `]
 })
 
 export class AppComponent {
     public title = "Tour of Heroes";
     public heroes = HEROES;
-    public hero = this.heroes[0];
+    public selectedHero: IHero;
+
+    onSelect(hero: IHero) {
+        this.selectedHero = hero;
+    }
 }
